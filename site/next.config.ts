800d24081f6abba3bfc8c0539a7cf789678ed1b2
Brandon Hopkins <brandon@caltechweb.com>
2026-07-08 18:51:12 -0700

    Revert "Inline CSS to eliminate render-blocking stylesheet request"
    This reverts commit a444cd4032a5b12ac34cbfae5014080bfdbf7042.

--- site/next.config.ts
+++ site/next.config.ts
@@ -3,9 +3,6 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
-  experimental: {
-    inlineCss: true,
-  },
   turbopack: {
     root: path.join(__dirname),
   },
